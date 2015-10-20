3331c47c76afc49de4c84035a59447f018c718cc
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2015-10-20 18:17:40 -0400

    Added check functions to verify query parameters

--- assets/js/pages/queryIndex.js
+++ assets/js/pages/queryIndex.js
@@ -3,7 +3,7 @@ var curTable;
 var curAttr = [];
 
 
-
+//Prints all DBs from db_info.js
 Object.keys(dbs).forEach(function(db){
 
 	var radioBtn = $('<input type="radio" name="db" id="db" value='+db+' >'+db+'</input>');
@@ -12,7 +12,7 @@ Object.keys(dbs).forEach(function(db){
 
 })
 
-
+//When a DB is selected, prints all tables for that DB, from db_info.js
 $("input[name='db']:radio").on('change',function(){
 	curDB = $(this).val();
 
@@ -28,11 +28,8 @@ $("input[name='db']:radio").on('change',function(){
 	})
 })
 
+//When a table is selected, prints all the fields for that table, from db_info.js
 $("#table_list").on('change',function(){
-	console.log("hello");
-
-
-
 
 	var chk = $('#table_list input:radio:checked');
  	curTable = chk.attr('value');
@@ -46,7 +43,7 @@ $("#table_list").on('change',function(){
 
 })
 
-
+//Jquery to execute query. Gets all selected attributes
 $("#executeQuery").on('click', function(){
 
 
@@ -61,7 +58,11 @@ $("#executeQuery").on('click', function(){
 
 	var params = {db:curDB,table:curTable,attr:curAttr};
 
-	execQuery(params);
+	//Run function to error-check parameters based on the DB and Table
+	if(checkParams(params)){
+		execQuery(params);		
+	}
+
 
 
 })
@@ -87,5 +88,65 @@ function execQuery(params){
 		console.log("data",dataFromServer);
 
 	})
+
+}
+
+//Check the parameters based on the DB and table
+//Returns true if all parameters have been verified, false otherwise
+function checkParams(params){
+
+	//Set default to true
+	var paramsStatus = true;
+
+
+	//Check DB
+	paramsStatus = checkDB(params.db);
+
+	//Check table
+	paramsStatus = checkTable(params.db,params.table);
+
+	//Check attributes
+	paramsStatus = checkAttr(params);
+
+
+	return paramsStatus;
+
+
+}
+
+//Checks to make sure DB is a legit DB or not
+//Takes just a DB
+//Returns a boolean
+function checkDB(db){
+
+	//Default to true
+	var dbStatus = true;
+
+	return dbStatus;
+}
+
+
+//Checks to make sure given DB contains the given table
+//Takes DB and Table
+//Returns a boolean
+function checkTable(db,table){
+
+	//Default to true
+	var tableStatus = true;
+
+	return tableStatus;
+
+
+}
+
+//Checks attributes
+//Takes all params
+//Returns a boolean
+function checkAttr(params){
+
+	//default to true
+	var attrStatus = true;
+
+	return attrStatus;
 
 }
