f9c41907ab21a2bac652570c905d29a7588c4aaf
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2015-10-22 21:46:49 -0400

    added jsonToCsv function

--- api/controllers/Geo_Wtp_Controller.js
+++ api/controllers/Geo_Wtp_Controller.js
@@ -1,14 +1,52 @@
 module.exports = {
 
 	'Wtp_data_issues': function(req,res){
-		console.log(req.param('params'));
-		var finalQuery = 'Select * from wtp_data_issues LIMIT 100';
+
+
+		// var params = req.param('params');
+		// console.log("params: ",params);
+
+		// params = JSON.parse(params);
+
+		// var query = 'Select ';
+		// var queryTable = ' from wtp_data_issues';
+
+		// //Get attributes for select statement
+		// var queryAttr = params.attr;
+		// console.log("attr: ",queryAttr);
+		// //Check for 'all'
+		// if(queryAttr[0] != 'all'){
+		// 	//Add attributes to query
+		// 	queryAttr.forEach(function(attribute){
+		// 		query = query + '`' + attribute + '`' + ',';
+		// 	})		
+
+		// 	//Remove last , since it is unnecessary
+		// 	query = query.substring(0, queryAttr.length - 1);
+
+		// }
+		// else{
+		// 	query = query + '*';
+		// }
+
+
+
+
+
+		// query = query + queryTable;
+
+		// //For TESTING PURPOSES ONLYYYYY
+		// var queryEnd = " LIMIT 100";
+
+
+		// var finalQuery = query + queryEnd;
 
 		console.log("In the controller wtp_data_issues");
 
+		finalQuery = "Select * from wtp_data_issues LIMIT 10";
 
 		//res.json({data:"NOTHING"});
-
+		console.log(finalQuery);
 		Geo_Wtp.query(finalQuery,null,function(err,data){
 			console.log("Error",err);
 			res.json(data);
