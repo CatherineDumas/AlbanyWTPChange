b94865aa3da882a699945ad77fcdcbb929efcb75
Margaret Dubowsky <dubowsky.m@gmail.com>
2015-11-05 21:59:09 -0500

    Added css to importer.css file and added a tsv button

--- assets/js/pages/queryIndex.js
+++ assets/js/pages/queryIndex.js
@@ -44,7 +44,7 @@ $("#table_list").on('change',function(){
 })
 
 //Jquery to execute query. Gets all selected attributes
-$("#executeQuery").on('click', function(){
+$("csv").on('click', function(){
 
 
 
@@ -65,14 +65,14 @@ $("#executeQuery").on('click', function(){
 
 	//Run function to error-check parameters based on the DB and Table
 	if(checkParams(params)){
-		execQuery(params);		
+		csvQuery(params);		
 	}
 
 
 
 })
 
-function execQuery(params){
+function csvQuery(params){
 	console.log(params)
 	// params.db = params.db.split(' ').join('_');
 	// params.table = params.table.split(' ').join('_');
@@ -96,6 +96,61 @@ function execQuery(params){
 	})
 
 }
+
+//Jquery to execute query. Gets all selected attributes
+$("tsv").on('click', function(){
+
+
+
+
+	curAttr = $('.attribute:checkbox:checked').map(function() {
+	    return this.value;
+	}).get();
+
+
+	console.log(curAttr);
+
+	var params = {db:curDB,table:curTable,attr:curAttr};
+
+
+	if(params.attr == null){
+		params.attr = "all";
+	}
+
+	//Run function to error-check parameters based on the DB and Table
+	if(checkParams(params)){
+		tsvQuery(params);		
+	}
+
+
+
+})
+
+function tsvQuery(params){
+	console.log(params)
+	// params.db = params.db.split(' ').join('_');
+	// params.table = params.table.split(' ').join('_');
+
+	// console.log(params)
+
+	var url  = dbs[params.db]['tables'][params.table].route
+	console.log("URL in execQuery", url);
+
+	var fileName = params.db + params.table;
+
+
+
+	d3.json(url)
+	.post(JSON.stringify({params:params}),function(err,dataFromServer){
+
+
+
+		console.log("data",dataFromServer);
+		JSONToTSVConvertor(dataFromServer, fileName, true);
+	})
+
+}
+
 
 //Check the parameters based on the DB and table
 //Returns true if all parameters have been verified, false otherwise
@@ -229,4 +284,76 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+}
+
+function JSONToTSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    
+    var tsv = '';    
+    //Set Report title in first row or line
+    
+    tsv += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+        
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+        
+        //append Label row with line break
+        tsv += row + '\r\n';
+    }
+    
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+        
+        //add a line break after each row
+        tsv += row + '\r\n';
+    }
+
+    if (tsv == '') {        
+        alert("Invalid data");
+        return;
+    }   
+    
+    //Generate a file name
+    var fileName = ReportTitle;
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");   
+    
+    //Initialize file format you want tsv or xls
+    var uri = 'data:text/tsv;charset=utf-8,' + escape(tsv);
+    
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".tsv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
 }
