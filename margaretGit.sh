7fb8b7eca2925f0da40bbf0b2a9a22f708eca0de
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2015-11-11 12:51:34 -0500

    Changed shell scripts to modify url as well

--- margaretGit.sh
+++ margaretGit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #NUM_THREADS	ITERATIONS
 
+https://MargaretDubowsky@github.com/RyanDubowsky/AlbanyWTPChange.git 
 git config user.name "Margaret Dubowsky"
 git config user.email "dubowsky.m@gmail.com"
--- ryanGit.sh
+++ ryanGit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #NUM_THREADS	ITERATIONS
 
+git remote set-url origin https://RyanDubowsky@github.com/RyanDubowsky/AlbanyWTPChange.git 
 git config user.name "Ryan Dubowsky"
 git config user.email "r.k.dubowsky@gmail.com"
