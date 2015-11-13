8757674406d361af64dd167e1de9b4dc65b0a867
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2015-11-12 22:40:08 -0500

    Can get the value of every where clause, and the button (range) value for time

--- assets/js/dependencies/paramValidation.js
+++ assets/js/dependencies/paramValidation.js
@@ -1,8 +1,10 @@
 //Check the parameters based on the DB and table
 //Returns true if all parameters have been verified, false otherwise
+//Need to now look for where clauses.
 function checkParams(params){
 	//Set default to true
 	var paramsStatus = true;
+	var whereParams = [];
 
 	//Check DB
 	paramsStatus = checkDB(params.db);
@@ -13,7 +15,41 @@ function checkParams(params){
 	//Check attributes
 	paramsStatus = checkAttr(params);
 
-	return paramsStatus;
+	//Need to add something to params
+	whereParams = getWhere(params.attr);
+
+	return false;
+}
+
+//Takes in an array of attributes
+function getWhere(attributes){
+
+	console.log(attributes);
+
+	attributes.forEach(function(attr){
+
+		var curAttr = $("#"+attr+"_where")
+		//If length is 0, it does not have a where field
+		if(curAttr.length == 0){
+
+		}
+		else{
+			//In here is all the attributes that have a where field
+
+			//There will be a radio button along with the input for time
+			if(dbs[curDB]['tables'][curTable]['fields'][attr].type == "time"){
+				console.log(curAttr[0].value);	
+				var buttonValue = $("input:radio[name='dateRange"+attr+"']:checked").val();	
+				console.log(buttonValue);	
+			}
+			else{
+
+			}
+
+
+		}
+	})
+
 }
 
 //Checks to make sure DB is a legit DB or not
